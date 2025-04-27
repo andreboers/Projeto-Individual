044d21926ce0c1e5ab406a0d72a75a1526d827fc
andreboers <andre.lsantos@sptech.school>
2025-04-26 22:42:12 -0300

    Cadastro e Login funcionando com o Banco de Dados

--- src/database/database_gta.sql
+++ src/database/database_gta.sql
@@ -8,3 +8,10 @@ nome varchar(100),
 email varchar(50),
 senha varchar(50)
 );
+
+insert into usuario (nome, email, senha) values
+('André','andre@gmail.com','123');
+
+select * from usuario;
+
+delete from usuario where id = '2';
