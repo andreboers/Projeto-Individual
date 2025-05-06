c8184badccd8dc82d16e93beb603f28b49d087d9
andreboers <andre.lsantos@sptech.school>
2025-05-05 22:22:35 -0300

    Estilizando a página Home e ajustando os campos das tabelas do Banco de Dados

--- src/database/database_minetech.sql
+++ src/database/database_minetech.sql
@@ -4,9 +4,9 @@ use minetech;
 
 create table usuario(
 id int primary key auto_increment,
-nome varchar(100),
-email varchar(50) unique,
-senha varchar(50)
+nome varchar(100) not null,
+email varchar(50) unique not null,
+senha varchar(50) not null
 );
 
 insert into usuario (nome, email, senha) values
